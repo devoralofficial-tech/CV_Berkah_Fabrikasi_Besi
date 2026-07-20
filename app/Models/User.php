e96efb86924fa0405b35582a12543bcfa52f07fa
Azmi <nauafalazmimukhlis@gmail.com>
2026-07-21 05:54:09 +0700

    Modify app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,46 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function inventoryLogs()
+    {
+        return $this->hasMany(InventoryLog::class, 'created_by');
+    }
+
+    public function sales()
+    {
+        return $this->hasMany(Sale::class, 'created_by');
+    }
+
+    public function stockOpnames()
+    {
+        return $this->hasMany(StockOpname::class, 'created_by');
     }
 }
